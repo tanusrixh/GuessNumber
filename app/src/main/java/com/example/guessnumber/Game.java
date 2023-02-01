00aeba88b62c20d1a439aea9ef507a0462d35381
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-02-01 09:24:32 -0500

    new changes

--- app/src/main/java/com/example/guessnumber/Game.java
+++ app/src/main/java/com/example/guessnumber/Game.java
@@ -6,11 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import java.util.Random;
+import android.widget.Toast;
 
 public class Game extends AppCompatActivity {
     Button instructions;
+    Random rand = new Random();
+
+    int result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int max = 11;
+        result = rand.nextInt(max);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         instructions = findViewById(R.id.instructions);
@@ -22,5 +31,38 @@ public class Game extends AppCompatActivity {
                 startActivity(instructionIntent);
             }
         });
+
+
     }
+
+    public void makeToast(String str)
+    {
+        Toast.makeText(Game.this,str, Toast.LENGTH_LONG).show();
+    }
+
+    public void clickFunction(View view)
+    {
+        int userGuessing;
+
+        EditText variable = findViewById(R.id.editId);
+        userGuessing = Integer.parseInt(variable.getText().toString());
+        if (userGuessing <= result - 2) {
+
+            makeToast("Hint: The number is higher");
+        }
+        else if (userGuessing >= result +2) {
+            makeToast("Hint: The number is lower");
+        }
+        else if (userGuessing == result +1) {
+            makeToast("Hint: Almost There! Just a little higher");
+        }
+        else if (userGuessing == result -1) {
+            makeToast("Hint: Almost There! Just a little lower");
+        }
+        else {
+            makeToast("Congratulations!");
+        }
+    }
+
+
 }
