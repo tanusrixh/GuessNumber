cfd5f0c061424677f7cee9242cad66ed26da1761
tanusrixh <118092429+tanusrixh@users.noreply.github.com>
2023-02-01 08:20:10 -0500

    commit 2

--- app/src/main/java/com/example/guessnumber/Game.java
+++ app/src/main/java/com/example/guessnumber/Game.java
@@ -8,10 +8,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class Game extends AppCompatActivity {
-
+    Button instructions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        instructions = findViewById(R.id.instructions);
+
+        instructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent instructionIntent = new Intent(Game.this,Instructionfromgame.class);
+                startActivity(instructionIntent);
+            }
+        });
     }
 }
